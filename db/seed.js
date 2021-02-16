a0199170ed2a18a3e706147c04396f50c837424c
jess <missmunks@gmail.com>
2021-02-15 22:14:43 -0800

    module 2 section 2

--- db/seed.js
+++ db/seed.js
@@ -1,7 +1,20 @@
 
-const { client, getAllUsers } = require('./index');
+const { client, getAllUsers, createUser, updateUser } = require('./index');
 
-
+async function createInitialUsers() {
+  try {
+    console.log("starting to create users....");
+    const albert = await createUser({ username: 'albert', password: 'bertie99', name: 'albertk', location: 'ny'});
+    const sandra = await createUser({username: 'sandra', password: 'glamgal', name: 'sandrah', location: 'texas'});
+    const glamgal = await createUser({username: 'glamgal', password: 'glamgal2', name: 'glamgal2', location: 'vegas'});
+    console.log(albert);
+    console.log(sandra)
+    console.log("finished creating users!");
+  }catch(error) {
+    console.error("error creating users!");
+    throw error;
+  }
+}
 
 async function dropTables(){
   try{
@@ -26,7 +39,10 @@ async function createTables(){
       CREATE TABLE users (
         id SERIAL PRIMARY KEY,
         username varchar(255) UNIQUE NOT NULL,
-        password varchar(255) NOT NULL
+        password varchar(255) NOT NULL,
+        name VARCHAR(255) NOT NULL,
+        location VARCHAR(255) NOT NULL,
+        active BOOLEAN DEFAULT true
       );
     `);
     console.log("Finished building tables!");
@@ -40,8 +56,11 @@ async function rebuildDB(){
 
   try{
     client.connect();
+
     await dropTables();
     await createTables();
+    await createInitialUsers();
+    await updateUser();
   } catch (error) {
     throw error;
   } 
@@ -50,14 +69,21 @@ async function rebuildDB(){
 async function testDB() {
   try {
     console.log("Starting to test database...");
-
+    console.log("calling getAllUsers")
     const users = await getAllUsers();
-    console.log("getAllUsers:", users);
+    console.log("result:", users);
 
-    console.log("Finished database tests!");
+    console.log("Calling updateUser on users[0]")
+    const updateUserResult = await updateUser(users[0].id, {
+      name: "Newname Sogood",
+      location: "lesterville, KY"
+    });
+    console.log("Result:", updateUserResult);
+    console.log("finished database tests!")
   } catch (error) {
     console.error(error);
     console.error("Error testing database!");
+
     throw error;
   } 
   }
